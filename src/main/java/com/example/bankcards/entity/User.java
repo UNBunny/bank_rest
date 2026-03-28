5b0d74a797177bd9c7be76a84ee006b0456996e6
UNBunny <nikitakuharchuk07@gmail.com>
2026-03-28 10:15:00 +0600

    feat: add User entity and Role enum

---
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.bankcards.entity;
+
+import com.example.bankcards.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
